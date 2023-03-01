ec27f7c06a7e85803503a584a1b3c933c127f17b
panchitowok2 <francisco.torres@est.fi.uncoma.edu.ar>
2023-03-01 14:23:59 -0500

    Primer commit, cree la variable de entorno para conectar con la BD

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import { MongoClient, ServerApiVersion } from 'mongodb';
 const uri = process.env.DATABASE_URL;
+console.log('Esto se trae el env: '+uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
